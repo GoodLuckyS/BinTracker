864b6d9eef659732751f0b93be1b47569cb80286
GoodLuckyS <xxgoodluckyxx@gmail.com>
2022-12-31 08:48:21 +0300

    Load Project

--- app/src/main/java/com/goodluckys/binlistapp/presentation/screens/tabs/HistoryViewModel.kt
+++ app/src/main/java/com/goodluckys/binlistapp/presentation/screens/tabs/HistoryViewModel.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class HistoryViewModel@Inject constructor(
-    private val getHistoryListUseCase: GetHistoryListUseCase,
+    getHistoryListUseCase: GetHistoryListUseCase,
     private val getCardInfoUseCase: GetCardInfoUseCase,
     private val deleteCardInfoUseCase: DeleteCardInfoUseCase
 ) : ViewModel() {
